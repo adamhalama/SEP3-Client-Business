034ce3f5d1b4081d3b442776e5979916d4878edf
Jerry Cao <q1063128177@gmail.com>
2021-11-01 19:41:15 +0100

    modify blazor part1

---
+++ BlazorClientServer/CarRentalClientServer/Data/CarList.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalClientServer.Models;
+using System.Collections.Generic;
+
+namespace CarRentalClientServer.Data
+{
+    public interface CarList
+    {
+        IList<Car> GetCars();
+        void AddCar(Car car);
+        void RemoveCar(int carId);
+        void UpdateCar(Car car);
+        Car GetSpcificCar (int carId);
+    }
+}
---
+++ BlazorClientServer/CarRentalClientServer/Data/CarListJson.cs
@@ -0,0 +1,62 @@
+﻿using CarRentalClientServer.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace CarRentalClientServer.Data
+{
+    public class CarListJson : CarList
+    {
+        private string carFile = "car.json";
+        private IList<Car> cars;
+
+        public CarListJson()
+        {
+            if (!File.Exists(carFile))
+            {
+                //Seed();
+                WriteTodosToFile();
+            }
+            else
+            {
+                string content = File.ReadAllText(carFile);
+                cars = JsonSerializer.Deserialize<List<Car>>(content);
+            }
+        }
+
+        private void WriteTodosToFile()
+        {
+            string todoAsJson = JsonSerializer.Serialize(cars);
+            File.WriteAllText(carFile, todoAsJson);
+        }
+        public void AddCar(Car car)
+        {
+            int max = cars.Max(car => car.Id);
+            car.Id = (++max);
+
+            cars.Add(car);
+            WriteTodosToFile();
+        }
+
+        public IList<Car> GetCars()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Car GetSpcificCar(int carId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveCar(int carId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCar(Car car)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
