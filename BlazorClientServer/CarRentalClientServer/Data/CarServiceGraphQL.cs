60655c143c2ae4c92d65b946675f1762c141b155
hadamhej <janadam.h@hotmail.sk>
2021-11-19 11:35:20 +0100

    working walking skeleton with GraphQL

--- BlazorClientServer/CarRentalClientServer/Data/CarServiceGraphQL.cs
+++ BlazorClientServer/CarRentalClientServer/Data/CarServiceGraphQL.cs
@@ -1,19 +1,104 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading.Tasks;
+using CarRentalClientServer.Data.Responses;
 using CarRentalClientServer.Models;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
 
 namespace CarRentalClientServer.Data
 {
     public class CarServiceGraphQL : ICarService
     {
+        private GraphQLHttpClient graphQlClient
+            = new GraphQLHttpClient("https://localhost:5010/graphql", new NewtonsoftJsonSerializer());
+
         public string SendMessage(string message)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<IList<Car>> GetCarsAsync()
+        public async Task<IList<Car>> GetCarsAsync()
         {
-            throw new System.NotImplementedException();
+            string query = @"
+            query
+            AllCars
+            {
+                allCars
+                {
+                    id
+                    name
+                    model
+                }
+            }
+            ";
+
+            var variables = new
+            {
+                id = 1,
+                name = "Jebo"
+            };
+
+            var request = MakeGraphQLRequest(query, "AllCars");
+
+
+            try
+            {
+                //showing what was sent
+                Console.WriteLine(JsonSerializer.Serialize(request,
+                    new JsonSerializerOptions
+                    {
+                        WriteIndented = true
+                    }));
+
+                
+                var graphQLResponse = await graphQlClient.SendQueryAsync<AllCarsResponse>(request);
+                
+                //showing serialized response
+                Console.WriteLine("serialised response:");
+                Console.WriteLine(JsonSerializer.Serialize(graphQLResponse,
+                    new JsonSerializerOptions
+                    {
+                        WriteIndented = true
+                    }));
+
+                return graphQLResponse.Data.AllCars;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine(e.InnerException.Message);
+                }
+                throw;
+            }
+        }
+
+        private static GraphQLRequest MakeGraphQLRequest(string query, string operationName, object variables)
+        {
+            GraphQLRequest request;
+            request = new GraphQLRequest
+            {
+                Query = query,
+                OperationName = operationName,
+                Variables = variables
+            };
+            return request;
+        }
+
+        private static GraphQLRequest MakeGraphQLRequest(string query, string operationName)
+        {
+            GraphQLRequest request;
+            request = new GraphQLRequest
+            {
+                Query = query,
+                OperationName = operationName
+            };
+            return request;
         }
 
         public Car AddCar(Car car)
--- BlazorClientServer/CarRentalClientServer/Startup.cs
+++ BlazorClientServer/CarRentalClientServer/Startup.cs
@@ -29,7 +29,7 @@ namespace CarRentalClientServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             
-            services.AddSingleton<ICarService, CarServiceSockets>();
+            services.AddScoped<ICarService, CarServiceGraphQL>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
