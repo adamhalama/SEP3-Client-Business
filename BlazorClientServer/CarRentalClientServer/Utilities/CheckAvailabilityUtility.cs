c7e0584baf89688c017abc07f1594f9252da176d
hadamhej <janadam.h@hotmail.sk>
2021-12-15 23:06:10 +0100

    Created a CheckAvailabilityUtility.cs, should be moved to the server though.

---
+++ BlazorClientServer/CarRentalClientServer/Utilities/CheckAvailabilityUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using CarRentalClientServer.Data;
+
+namespace CarRentalClientServer.Utilities
+{
+    public class CheckAvailabilityUtility
+    {
+        public static async Task<bool> CheckForAvailability(long dateStart, long dateEnd, long vehicleId, IVehicleService vehicleService)
+        {
+            bool isValid = false;
+            var availableVehicles = await vehicleService.GetAvailableVehiclesAsync(dateStart, dateEnd);
+            foreach (var veh in availableVehicles)
+            {
+                if (veh.Id == vehicleId)
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+
+            return isValid;
+        }
+    }
+}
