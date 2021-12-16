0ac29fc9178053e5ce839dca41cb32d52820b298
hadamhej <47830178+hadamhej@users.noreply.github.com>
2021-12-16 17:47:43 +0100

    Deleting CarRentalAPI
    This package has its own repository now, here you can find it up to date: https://github.com/hadamhej/CarRental-SpringBoot-REST-API

--- JavaAPIDB/CarRentalAPI/src/main/java/com/SEP3/CarRentalAPI/DBRepository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SEP3.CarRentalAPI.DBRepository;
-
-import com.SEP3.CarRentalAPI.Model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>
-{
-    Customer findByEmail(String email);
-}
